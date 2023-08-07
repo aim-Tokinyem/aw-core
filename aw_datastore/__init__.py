3dffdc52a296bcc5a89d8afef1e8e0ee9650d675
Rofifah Nurul Annisa <rofifahnurul00@gmail.com>
2023-08-07 11:05:11 +0700

    Delete library pyodbc

--- aw_datastore/__init__.py
+++ aw_datastore/__init__.py
@@ -14,6 +14,7 @@ def get_storage_methods() -> Dict[str, Callable[..., storages.AbstractStorage]]:
         SqliteStorage.sid: SqliteStorage,
         PostgreyStorage.sid: PostgreyStorage,
     }
+    print("methods={methods}")
     return methods
 
 
